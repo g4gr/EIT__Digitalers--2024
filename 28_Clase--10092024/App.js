53794dc65c26b10768b67ff3179be60fe8e49270
Gaby <76602751+g4gr@users.noreply.github.com>
2024-09-10 19:30:04 -0300

    ASSETS(ALL): Agrego los archivos de la clase de hoy

---
+++ 28_Clase--10092024/App.js
@@ -0,0 +1,39 @@
+// --- Botones ---
+// TODO: 
+// 1. "Ir a buscar" la tarea al formulario de tareas.
+// 2. Cuando tenga la tarea, debo poder: 
+// 2.A. Crearla, asignarle una fecha, asignarle una prioridad y sumarla a la pila de tareas.
+// 2.B. Limpiar el formulario.
+
+
+// --- Tareas ---
+// TODO: 
+// 1. Crear una clase que contenta el modelo inicial de la tareas.
+// 1.A. Debe tener : Fecha, Contenido, Prioridad.
+
+
+// --- Editar ---
+// TODO:
+// 1. Editar la tarea.
+// 2. Completar la tarea.
+// 3. Eliminar la tarea.
+// 4. Seleccionar la tarea.
+
+// Esperar a que el DOM esté completamente cargado
+
+// Función para agregar una tarea
+function addTask() {
+}
+
+// Función para seleccionar una tarea
+function selectTask(taskItem) {
+}
+
+// Función para editar una tarea
+function editTask() {
+}
+
+// Función para eliminar una tarea
+function deleteTask() {
+}
+
